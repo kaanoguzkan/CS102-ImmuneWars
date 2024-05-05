cb6120cdaf6809f2ba7a5c98fd7f505fc91d388c
Sırrı Kaan Oğuzkan <kaan.oguzkan@ug.bilkent.edu.tr>
2024-05-06 01:22:13 +0300

    changed the edges and nodes to be more visible

--- ImmuneWars/core/src/com/immunewars/game/screens/MainMap/Edge/EdgeActor.java
+++ ImmuneWars/core/src/com/immunewars/game/screens/MainMap/Edge/EdgeActor.java
@@ -1,5 +1,58 @@
 package com.immunewars.game.screens.MainMap.Edge;
 
-public class EdgeActor {
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.immunewars.game.screens.MainMap.Node.NodeActor;
+
+public class EdgeActor extends Actor 
+{
+    private NodeActor nodeActor1, nodeActor2;
+
+    public EdgeActor(NodeActor nodeActor1, NodeActor nodeActor2)
+    {
+        this.nodeActor1 = nodeActor1;
+        this.nodeActor2 = nodeActor2;
+    }
+    
+    @Override
+    public void draw(Batch batch, float parentAlpha)
+    {
+        ShapeRenderer shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.BLACK); // Set desired color
+        shapeRenderer.line(
+            nodeActor1.getX() + nodeActor1.getWidth() / 2, 
+            nodeActor1.getY() + nodeActor1.getHeight() / 2,
+            nodeActor2.getX() + nodeActor2.getWidth() / 2,
+            nodeActor2.getY() + nodeActor2.getHeight() / 2
+        );
+        shapeRenderer.end();
+    }
+
+    public NodeActor getNodeActor1() 
+    {
+        return nodeActor1;
+    }
+
+    public void setNodeActor1(NodeActor nodeActor1) 
+    {
+        this.nodeActor1 = nodeActor1;
+    }
+
+    public NodeActor getNodeActor2() 
+    {
+        return nodeActor2;
+    }
+
+    public void setNodeActor2(NodeActor nodeActor2) 
+    {
+        this.nodeActor2 = nodeActor2;
+    }
+
     
 }
--- ImmuneWars/core/src/com/immunewars/game/screens/MainMap/Node/NodeActor.java
+++ ImmuneWars/core/src/com/immunewars/game/screens/MainMap/Node/NodeActor.java
@@ -1,5 +1,48 @@
 package com.immunewars.game.screens.MainMap.Node;
 
-public class NodeActor {
-    
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+
+public class NodeActor extends Actor
+{
+    private Texture texture;
+    private Node nodeData;
+
+    public NodeActor(Texture texture, Node nodeData)
+    {
+        this.texture = texture;
+        this.nodeData = nodeData;
+
+        setWidth(texture.getWidth());
+        setHeight(texture.getHeight());
+
+        setPosition(getOriginX(), getOriginY());
+
+        addListener(new InputListener()
+        {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+            {
+                // pops up a dialog box with the node's information 
+                 
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha)
+    {
+        batch.draw(texture, getX(), getY());
+    }
+
+    public Node getNodeData()
+    {
+        return nodeData;
+    }
+
+
 }
