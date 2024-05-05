2190d32f6697ddbe6de46bc3ebc6badb93811bc1
Sırrı Kaan Oğuzkan <kaan.oguzkan@ug.bilkent.edu.tr>
2024-05-05 17:14:31 +0300

    updated the node and edge classes

--- ImmuneWars/core/src/com/immunewars/game/screens/MainMap/Edge.java
+++ ImmuneWars/core/src/com/immunewars/game/screens/MainMap/Edge.java
@@ -1,5 +1,34 @@
 package com.immunewars.game.screens.MainMap;
 
-public class Edge {
-    
+public class Edge 
+{
+    private Node start;
+    private Node end;
+     
+    public Edge(Node start, Node end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Node getStart() 
+    {
+        return start;
+    }
+
+    public void setStart(Node start) 
+    {
+        this.start = start;
+    }
+
+    public Node getEnd() 
+    {
+        return end;
+    }
+
+    public void setEnd(Node end) 
+    {
+        this.end = end;
+    }
+
 }
--- ImmuneWars/core/src/com/immunewars/game/screens/MainMap/Node.java
+++ ImmuneWars/core/src/com/immunewars/game/screens/MainMap/Node.java
@@ -1,5 +1,134 @@
 package com.immunewars.game.screens.MainMap;
 
-public class Node {
+import java.util.ArrayList;
+
+public class Node 
+{
+    private int id; 
+    private int x;
+    private int y;
+    private String name;
+    private int type;
+    private ArrayList<Node> neighbors;
+    private int weight;
+
+    public Node(int id, int x, int y, String name, int type, int[] neighbors, int weight)
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.type = type;
+        this.neighbors = neighbors;
+        this.weight = weight;
+    }
+
+    public int getId() 
+    {
+        return id;
+    }
+
+    public void setId(int id) 
+    {
+        this.id = id;
+    }
+
+    public int getX() 
+    {
+        return x;
+    }
+
+    public void setX(int x) 
+    {
+        this.x = x;
+    }
+
+    public int getY() 
+    {
+        return y;
+    }
+
+    public void setY(int y) 
+    {
+        this.y = y;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public int getType() 
+    {
+        return type;
+    }
+
+    public void setType(int type) 
+    {
+        this.type = type;
+    }
+
+    public int[] getNeighbors() 
+    {
+        return neighbors;
+    }
+
+    public void setNeighbors(int[] neighbors) 
+    {
+        this.neighbors = neighbors;
+    }
+
+    public int getWeight() 
+    {
+        return weight;
+    }
+
+    public void setWeight(int weight) 
+    {
+        this.weight = weight;
+    }
+
+    public void addNeighbor(Node neighbor)
+    {
+        neighbors.add(neighbor);
+    }
+
+    public void removeNeighbor(Node neighbor)
+    {
+        neighbors.remove(neighbor);
+    }
+
+    public void clearNeighbors()
+    {
+        neighbors.clear();
+    }
+
+    public boolean isNeighbor(Node neighbor)
+    {
+        return neighbors.contains(neighbor);
+    }
+
+    public int getNeighborCount()
+    {
+        return neighbors.size();
+    }
+
+    public boolean equals(Node node)
+    {
+        return this.id == node.id;
+    }
+
+    public String toString()
+    {
+        return name;
+    }
+
     
+    
+
 }
